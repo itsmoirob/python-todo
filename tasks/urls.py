8da4619b976b7606f0a3e750f06e142ec5a3c98b
Robbie <rob.w.sherman@gmail.com>
2020-12-31 07:18:46 +0000

    Add delete task url

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete_task/<str:pk>/', views.deleteTask, name="delete_task"),
     path('', views.index, name="list")
 ]
