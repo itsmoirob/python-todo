a8629a0823de726166b43abd6b4d7d69dfbdab1b
Robbie <rob.w.sherman@gmail.com>
2020-12-31 07:22:20 +0000

    Update urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-    path('delete_task/<str:pk>/', views.deleteTask, name="delete_task"),
+    path('update/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete_task"),
     path('', views.index, name="list")
 ]
